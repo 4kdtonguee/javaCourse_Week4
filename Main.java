435baa390965335c8e9691703eeceddbd8763b17
Eduard Ailincai <eduardailincai2.0@gmail.com>
2023-07-31 16:15:27 +0300

    Main modified, println should work properly now

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Zoo zoo = new Zoo();
         while (true) {
-            System.out.println("1) Add a hedgehog, 2) Let them speak, 3) List animals, 0) Exit");
+            System.out.println("1) Add a hedgehog, 2) Let them speak, 3) List animals, 0) Exit \n");
             int option = sc.nextInt();
             sc.nextLine();
 
             switch (option) {
                 case 1:
-                    System.out.println("Give a name to the hedgehog:");
+                    System.out.println("Give a name to the hedgehog:\n");
                     String name = sc.nextLine();
                     Hedgehog hedgehog = new Hedgehog(name);
                     zoo.addAHedgehog(hedgehog);
@@ -19,7 +19,7 @@ public class Main {
                     break;
 
                 case 2:
-                    System.out.println("What should hedgehogs say?");
+                    System.out.println("What should hedgehogs say?\n");
                     String say = sc.nextLine();
                     try {
                         int number = Integer.parseInt(say);
@@ -36,7 +36,7 @@ public class Main {
                 case 0:
                     System.exit(0);
                 default:
-                    System.out.println("Invalid choice. Try again.");
+                    System.out.println("Invalid choice. Try again.\n");
                     break;
             }
         }
